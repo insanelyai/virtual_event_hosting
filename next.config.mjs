1eba9b831e4f060d3485886d15001c406832078c
= <waghoorehan03@gmail.com>
2024-09-15 18:13:06 +0530

    Navbar added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["logodix.com", "seeklogo.com", "images.unsplash.com"], // Add any other domains as needed
+  },
+};
 
 export default nextConfig;
